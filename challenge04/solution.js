a0d8e3e6464016efea827c5f385a5d54144c5e83
David Basto <davidbasto01@gmail.com>
2022-11-22 21:42:35 -0500

    update: refactor

--- challenge04/solution.js
+++ challenge04/solution.js
@@ -1,32 +1,16 @@
 const isValidPassword = (password) => {
-    let passwordDigits = password.toString().split('').reverse().map(Number);
-    let isValid = true;
-
-    for (let i=0; i < passwordDigits.length - 1; i++){
-        if(passwordDigits[i] < passwordDigits[i+1]) {
-            isValid = false;
-            break;
-        }
-    }
-    return isValid;
+    let orderedPassword = password.toString().split('').reverse().sort().join('');
+    return password.toString() === orderedPassword;
 }
 
 const RANGE = {
     min: 11098,
-    max: 98123
+    max: 98123,
 }
-let passwords = [];
 
-for(let pwd = RANGE.min; pwd <= RANGE.max; pwd++){
-
-    if(pwd.toString().match(/55/) == null) continue;
-
-    let validPassword = isValidPassword(pwd);
-    if(!validPassword) continue;
-
-    passwords.push(pwd);
-
-}
+const passwords = [...Array(RANGE.max).keys()].slice(RANGE.min).filter(password => {
+  return password.toString().includes('55') && isValidPassword(password);
+})
 
 //console.log("passwords", passwords);
 console.log("length =>", passwords.length);
